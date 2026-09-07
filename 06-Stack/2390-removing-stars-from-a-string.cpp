46ce436ab05433563b164e131e2be608c94900bd
oliveeah <olliebennettx@gmail.com>
2026-09-07 18:32:25 +0100

    leetcode 2390 completed

--- 06-Stack/2390-removing-stars-from-a-string.cpp
+++ 06-Stack/2390-removing-stars-from-a-string.cpp
@@ -1,1 +1,42 @@
 // LeetCode 2390: Removing Stars From a String
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    string removeStars(string s)
+    {
+        vector<char> chars{};
+        chars.reserve(s.size());
+
+        for (char c : s)
+        {
+            if (c == '*')
+            {
+                if (!chars.empty())
+                {
+                    chars.pop_back();
+                }
+            }
+            else
+            {
+                chars.emplace_back(c);
+            }
+        }
+        string rS{};
+        rS.reserve(chars.size());
+        for (char c : chars)
+        {
+            rS.push_back(c);
+        }
+
+        return rS;
+    }
+};
+
+int main()
+{
+    std::cout << Solution{}.removeStars("leet**cod*e") << '\n';
+}
