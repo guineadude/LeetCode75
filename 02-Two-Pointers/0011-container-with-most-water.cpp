dcc295276420ad7c029622ab3e8d7816fef4e903
oliveeah <olliebennettx@gmail.com>
2026-09-07 20:09:42 +0100

    dont container with most water leetcode 11

--- 02-Two-Pointers/0011-container-with-most-water.cpp
+++ 02-Two-Pointers/0011-container-with-most-water.cpp
@@ -1,1 +1,30 @@
 // LeetCode 11: Container With Most Water
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        size_t LB{0uz}, RB{height.size() - 1};
+        int mostWaterStored{};
+
+        while (LB < RB)
+        {
+            const int currentWaterStored{
+                min(height[LB], height[RB]) * static_cast<int>(RB - LB)}; // y * x
+            mostWaterStored = max(mostWaterStored, currentWaterStored);
+            height[LB] > height[RB] ? --RB : ++LB;
+        }
+
+        return mostWaterStored;
+    }
+};
+
+int main()
+{
+    vector<int> height{1, 8, 6, 2, 5, 4, 8, 3, 7};
+    cout << Solution{}.maxArea(height) << '\n';
+}
