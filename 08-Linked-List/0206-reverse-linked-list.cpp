1c32623d3fb99fa9f27b06b786186b67508bd26c
oliveeah <olliebennettx@gmail.com>
2026-09-09 20:03:12 +0100

    238 done

--- 08-Linked-List/0206-reverse-linked-list.cpp
+++ 08-Linked-List/0206-reverse-linked-list.cpp
@@ -1,1 +1,41 @@
 // LeetCode 206: Reverse Linked List
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        vector<int> answer(nums.size());
+
+        int cumulativeProd{1};
+
+        // L -> R
+        for (auto i{0uz}; i < nums.size(); ++i)
+        {
+            answer[i] = cumulativeProd;
+            cumulativeProd *= nums[i];
+        }
+
+        cumulativeProd = 1;
+
+        // R -> L
+        for (auto j{std::ssize(nums) - 1}; j >= 0; --j)
+        {
+            answer[j] *= cumulativeProd;
+            cumulativeProd *= nums[j];
+        }
+
+        return answer;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    vector<int> nums{1, 2, 3, 4};
+    solution.productExceptSelf(nums);
+    return 0;
+}
